1d2cfc35f4e9171a86e75f078795e00b93b68d9d
OmegaFox <Quyetnguyen.nkq.2002@gmail.com>
2025-01-04 17:21:19 +0700

    change color badge

--- scripts/widgets/blight_badge.lua
+++ scripts/widgets/blight_badge.lua
@@ -1,7 +1,7 @@
 local Badge = require "widgets/badge" -- badge template to be used 
 
 local blight_badge  = Class(Badge, function(self, owner)
-    Badge._ctor(self, nil, owner, { 225 / 255, 64 / 255, 0 / 255, 1 }) -- "nil" here is supposed to be the badge's art, but we couldn't figure it out. It'll use the default badge art without a icon.--  { 174 / 255, 21 / 255, 21 / 255, 1 } = colour in an rbg format
+    Badge._ctor(self, nil, owner, { 102 / 255, 99 / 255, 102 / 255, 1 })  -- "nil" here is supposed to be the badge's art, but we couldn't figure it out. It'll use the default badge art without a icon.--  { 174 / 255, 21 / 255, 21 / 255, 1 } = colour in an rbg format
     self.owner = owner
     self.num.max = TUNING.WYRMBANE_BLIGHT --This number is the max of the character's badge
     self.num.current = 0 
